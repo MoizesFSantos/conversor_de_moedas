c1ed63c016e7609887bab63dcf0ff822f053f24c
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2022-05-14 14:27:17 -0300

    first push

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -6,10 +6,45 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset('assets/money_icon.png'),
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+          child: Column(
+            children: [
+              Image.asset('assets/money_icon.png'),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: DropdownButton(
+                        items: [
+                          DropdownMenuItem(child: Text('Real')),
+                          DropdownMenuItem(child: Text('Dolar')),
+                        ],
+                        onChanged: (void value) {},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(flex: 2, child: TextField())
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Converter'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
